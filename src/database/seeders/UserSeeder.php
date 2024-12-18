5144abd93c3b14e790165285d3d276461517376c
jasen-devvv <janadevofficial@gmail.com>
2024-12-18 21:17:06 +0800

    feat: add new seeder
    - Add UserSeeder for user admin
    - Call UserSeeder & RolePermissionSeeder at DatabaseSeeder

---
+++ src/database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => 'admin123',
+        ]);
+        
+        $admin->assignRole('admin');
+    }
+}
