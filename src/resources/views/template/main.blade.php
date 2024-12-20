a37959d035c0571da2c52a327a72b8231cf9afb0
jasen-devvv <janadevofficial@gmail.com>
2024-12-20 20:05:40 +0800

    feat: add script section used @yield

--- src/resources/views/template/main.blade.php
+++ src/resources/views/template/main.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Dashboard - NiceAdmin Bootstrap Template</title>
+  <title>{{ $title }}</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -71,6 +71,9 @@
 
   <!-- Template Main JS File -->
   <script src="{{ asset('js/main.js') }}"></script>
+  
+  <!-- Custom JS -->
+  @yield('script')
 
 </body>
 
