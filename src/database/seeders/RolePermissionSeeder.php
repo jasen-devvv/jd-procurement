3bef555c3a426dc0ba716e9bc9713e558793e6ec
jasen-devvv <janadevofficial@gmail.com>
2024-12-26 18:20:05 +0800

    feat: add new permissions
    - Add edit and delete request at permissions requests
    - Add view, create, edit and delete at permissions suppliers
    - Add view, create, edit and delete at permissions products
    - Give new permissions for admin and staff

--- src/database/seeders/RolePermissionSeeder.php
+++ src/database/seeders/RolePermissionSeeder.php
@@ -14,18 +14,32 @@ class RolePermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        // Permissions
-        Permission::create(['name' => 'view requests']);
-        Permission::create(['name' => 'create requests']);
-        Permission::create(['name' => 'approve requests']);
-        Permission::create(['name' => 'reject requests']);
+        // Permissions Requests
+        Permission::create(['name' => 'view request']);
+        Permission::create(['name' => 'create request']);
+        Permission::create(['name' => 'edit request']);
+        Permission::create(['name' => 'delete request']);
+        Permission::create(['name' => 'approve request']);
+        Permission::create(['name' => 'reject request']);
+        
+        // Permissions Suppliers
+        Permission::create(['name' => 'view supplier']);
+        Permission::create(['name' => 'create supplier']);
+        Permission::create(['name' => 'edit supplier']);
+        Permission::create(['name' => 'delete supplier']);
+
+        // Permissions Products
+        Permission::create(['name' => 'view product']);
+        Permission::create(['name' => 'create product']);
+        Permission::create(['name' => 'edit product']);
+        Permission::create(['name' => 'delete product']);
 
         // Roles
         $admin = Role::create(['name' => 'admin']);
         $staff = Role::create(['name' => 'staff']);
 
         // Assign permissions to roles
-        $admin->givePermissionTo(['view requests', 'approve requests', 'reject requests']);
-        $staff->givePermissionTo(['view requests', 'create requests']);
+        $admin->givePermissionTo(['view request', 'approve request', 'reject request', 'view supplier', 'create supplier', 'edit supplier', 'delete supplier', 'view product', 'create product', 'edit product', 'delete product']);
+        $staff->givePermissionTo(['view request', 'create request', 'edit request', 'delete request', 'view supplier', 'view product']);
     }
 }
