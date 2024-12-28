59ac49063d68a24767c105fea2324be149ee9f31
jasen-devvv <janadevofficial@gmail.com>
2024-12-28 08:09:26 +0800

    feat: add script for fullcalendar

--- src/resources/views/template/main.blade.php
+++ src/resources/views/template/main.blade.php
@@ -63,6 +63,8 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
+  <script src="{{ asset('vendor/fullcalendar/index.global.js') }}"></script>
+  <script src="{{ asset('vendor/fullcalendar/index.global.min.js') }}"></script>
   <script src="{{ asset('vendor/apexcharts/apexcharts.min.js') }}"></script>
   <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
   <script src="{{ asset('vendor/chart.js/chart.umd.js') }}"></script>
