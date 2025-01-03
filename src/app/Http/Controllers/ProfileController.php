9eca270e054f0a16ac84a0f9667412b00b0c9033
jasen-devvv <janadevofficial@gmail.com>
2025-01-03 09:48:40 +0800

    feat: add controller profile
    - Method index for views blade profile.
    - Method update for updated data profile user.
    - Change Password.

---
+++ src/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Profile;
+use App\Enums\ProfileGender;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class ProfileController extends Controller
+{
+    public function index()
+    {
+        $genders = ProfileGender::cases();
+        $userId = Auth::id();
+        $profile = Profile::where('user_id', $userId)->first();
+
+        $data = [
+            'title' => 'Profile | E-Procurement',
+            'genders' => $genders,
+            'profile' => $profile
+        ];
+
+        return view('dashboard.profile', $data);
+    }
+
+    public function update(Request $request)
+    {
+        $validData = $request->validate([
+            'full_name' => 'nullable',
+            'about' => 'nullable',
+            'address' => 'nullable',
+            'gender' => 'nullable',
+            'phone' => 'nullable'
+        ]);
+
+        $userId = Auth::id();
+
+        $profile = Profile::updateOrCreate(['user_id' => $userId], $validData);
+        Profile::logActivity('updated');
+
+        return redirect()->route('profile');
+    }
+
+    public function change_password()
+    {
+        $validData = $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed',
+        ]);
+
+        $user = Auth::user();
+
+        if(!Hash::check($validData['old_password'], $user->password)) {
+            return redirect()->back();
+        }
+
+        $user->password = $validData['new_password'];
+        $user->save();
+        
+        return redirect()->route('profile');
+    }
+}
