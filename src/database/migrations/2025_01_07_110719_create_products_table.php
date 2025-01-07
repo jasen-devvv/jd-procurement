94fcb8697c02c157c678d3dc8de757de4a06b479
jasen-devvv <janadevofficial@gmail.com>
2025-01-07 12:07:18 +0800

    fix: rebuild old product migrations

--- src/database/migrations/2025_01_07_110719_create_products_table.php
+++ src/database/migrations/2025_01_07_110719_create_products_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('supplier_id')->constrained()->onDelete('cascade');
-
             $table->string('name');
             $table->text('description')->nullable();
             $table->decimal('price', 10, 2);
