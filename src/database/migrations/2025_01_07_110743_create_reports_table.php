eca6cc116c18c91a47e17d602b1904c9af33c8c6
jasen-devvv <janadevofficial@gmail.com>
2025-01-08 12:49:18 +0800

    fix: rename column
    - Column total_new_products to total_products.
    - Column total_new_suppliers to total_suppliers.

--- src/database/migrations/2025_01_07_110743_create_reports_table.php
+++ src/database/migrations/2025_01_07_110743_create_reports_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->date('range_start');
             $table->date('range_end');
             $table->integer('total_orders')->default(0);
-            $table->integer('total_new_products')->default(0);
-            $table->integer('total_new_suppliers')->default(0);
+            $table->integer('total_products')->default(0);
+            $table->integer('total_suppliers')->default(0);
             $table->unsignedBigInteger('top_product_id')->nullable();
             $table->unsignedBigInteger('top_supplier_id')->nullable();
             $table->timestamps();
