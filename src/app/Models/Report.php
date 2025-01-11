ff3a55ddc25d392362fbcfad024f7655411f53f5
jasen-devvv <janadevofficial@gmail.com>
2025-01-11 20:25:37 +0800

    fix: change field and add casts attribute
    - Change field range_start and range_end to date_start and date_end.
    - Add casts attribute for date_start and date_end to be able use format() at views.

--- src/app/Models/Report.php
+++ src/app/Models/Report.php
@@ -8,12 +8,19 @@ class Report extends Model
 {
     protected $fillable = [
         'name',
-        'range_start',
-        'range_end',
+        'date_start',
+        'date_end',
         'total_orders',
         'total_products',
         'total_suppliers',
         'top_product_id',
+        'top_product_total',
         'top_supplier_id',
+        'top_supplier_total',
+    ];
+
+    protected $casts = [
+        'date_start' => 'datetime',
+        'date_end' => 'datetime',
     ];
 }
