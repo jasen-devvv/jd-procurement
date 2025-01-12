d1a359e1133eb5018589b6bd9c21e9d876811b1f
jasen-devvv <janadevofficial@gmail.com>
2025-01-12 19:56:23 +0800

    feat: add new controller for reports
    - Name controller is ReportController.
    - Method index for show all reports.
    - Method detail for show details report.
    - Method pdf for print report to pdf.
    - Method excel for build data to xlsx.

---
+++ src/app/Http/Controllers/ReportController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Report;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Exports\ReportExport;
+use Carbon\Carbon;
+
+class ReportController extends Controller
+{
+    public function index()
+    {
+        $reports = Report::all();
+
+        $data = [
+            'title' => 'Report | E-Procurement',
+            'reports' => $reports
+        ];
+
+        return view('dashboard.report.index', $data);
+    }
+
+    public function detail(string $id)
+    {
+        $report = Report::with(['product', 'supplier'])->findOrFail($id);
+
+        $data = [
+            'title' => 'Report | E-Procurement',
+            'report' => $report
+        ];
+
+        return view('dashboard.report.detail', $data);
+    }
+    
+    
+    public function pdf(string $id)
+    {
+        $report = Report::with(['supplier', 'product'])->findOrFail($id);
+
+        $data = [
+            'title' => 'Print Report | E-Procurement',
+            'report' => $report
+        ];
+
+        $pdf = Pdf::loadView('dashboard.report.print', $data);
+        return $pdf->download('report.pdf');
+
+        return view('dashboard.report.print', $data);
+    }
+
+    public function excel(string $id)
+    {
+        $year = Carbon::now()->year;
+        $title = "Report Monthly " . $year;
+        return Excel::download(new ReportExport($id, $title), 'report.xlsx');
+    }
+}
