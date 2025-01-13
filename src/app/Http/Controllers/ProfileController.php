8103cc217a74ee42cde0371d57203e28251d592a
jasen-devvv <janadevofficial@gmail.com>
2025-01-13 21:34:38 +0800

    feat: add session message
    - Add session success message for update and change_password method.

--- src/app/Http/Controllers/ProfileController.php
+++ src/app/Http/Controllers/ProfileController.php
@@ -11,6 +11,9 @@ use Illuminate\Validation\Rule;
 
 class ProfileController extends Controller
 {
+    /**
+     * Display the specified profile user.
+     */
     public function index()
     {
         $genders = ProfileGender::cases();
@@ -26,14 +29,17 @@ class ProfileController extends Controller
         return view('dashboard.profile', $data);
     }
 
+    /**
+     * Update the specified profile user in storage.
+     */
     public function update(Request $request)
     {
         $validData = $request->validate([
-            'full_name' => 'nullable',
-            'about' => 'nullable',
-            'address' => 'nullable',
+            'name' => ['nullable'],
+            'about' => ['nullable'],
+            'address' => ['nullable'],
             'gender' => ['nullable', Rule::enum(ProfileGender::class)],
-            'phone' => 'nullable'
+            'phone' => ['nullable']
         ]);
 
         $userId = Auth::id();
@@ -41,26 +47,30 @@ class ProfileController extends Controller
         $profile = Profile::updateOrCreate(['user_id' => $userId], $validData);
         Profile::activity('updated');
 
-        return redirect()->route('profile');
+        return redirect()->route('profile')->with('success', 'Your profile has been successfully updated.');
     }
 
+    /**
+     * Change password the specified user in storage.
+     */
     public function change_password(Request $request)
     {
         $validData = $request->validate([
-            'old_password' => 'required',
-            'new_password' => 'required|confirmed',
+            'old_password' => ['required'],
+            'new_password' => ['required', 'confirmed'],
         ]);
 
         $user = Auth::user();
 
         if(!Hash::check($validData['old_password'], $user->password)) {
-            return redirect()->back();
+            return redirect()->back()->with('failed', 'The current password you entered is incorrect.');
         }
 
         $user->password = $validData['new_password'];
         $user->save();
+
         Profile::activity('changed password');
         
-        return redirect()->route('profile');
+        return redirect()->route('profile')->with('success', 'Your password has been successfully changed.');
     }
 }
