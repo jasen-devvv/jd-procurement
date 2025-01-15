92683da9b014bba44414eb86faaa12479aa6126c
jasen-devvv <janadevofficial@gmail.com>
2025-01-15 10:06:38 +0800

    feat: add nullable() so that data can be null

--- src/database/migrations/2025_01_07_152515_create_profiles_table.php
+++ src/database/migrations/2025_01_07_152515_create_profiles_table.php
@@ -14,11 +14,11 @@ return new class extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('name');
-            $table->text('about');
+            $table->string('name')->nullable();
+            $table->text('about')->nullable();
             $table->enum('gender', ['male', 'female', 'other']);
-            $table->string('address');
-            $table->string('phone');
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
             $table->timestamps();
         });
     }
