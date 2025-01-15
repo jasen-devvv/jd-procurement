74110844027ce374be34614892097d83e291b2e1
jasen-devvv <janadevofficial@gmail.com>
2025-01-15 08:41:15 +0800

    fix: rename data, change table header and data
    - Rename request to order.
    - Change Table header Request -> Product and Supplier -> Quantity.
    - Change $requests as $req -> $orders as $order.
    - Change all $req to $order.
    - Change $req->supplier->name to $order->product->name.
    - Change $req->supplier->name to $order->quantity.

--- src/resources/views/dashboard/index.blade.php
+++ src/resources/views/dashboard/index.blade.php
@@ -22,48 +22,6 @@
 	  <div class="@role('admin') col-lg-8 @endrole @role('staff') col-lg-6 @endrole">
 		<div class="row">
 
-		  <!-- Product Card -->
-		  <div class="@role('admin') col-xxl-4 @endrole @role('staff') col-xxl-6 @endrole col-md-6">
-			<div class="card info-card product-card">
-
-			  <div class="filter">
-				<a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-				<ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-				  <li class="dropdown-header text-start">
-					<h6>Filter</h6>
-				  </li>
-
-				  	<li><a class="dropdown-item" href="{{ route('dashboard', ['card' => 'product', 'period' => 'daily']) }}">Today</a></li>
-					<li><a class="dropdown-item" href="{{ route('dashboard', ['card' => 'product', 'period' => 'monthly']) }}">This Month</a></li>
-					<li><a class="dropdown-item" href="{{ route('dashboard', ['card' => 'product', 'period' => 'yearly']) }}">This Year</a></li>
-
-				</ul>
-			  </div>
-
-			  <div class="card-body">
-				<h5 class="card-title">Total Product <span>| {{ request()->get('period', 'daily') }}</span></h5>
-
-				<div class="d-flex align-items-center">
-				  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-					<i class="bi bi-box-fill"></i>
-				  </div>
-				  <div class="ps-3">
-					<h6>{{ $productCount }}</h6>
-					@role('admin')
-					<span class="{{ $productPercentageChange >= 0 ? 'text-success' : 'text-danger' }} small pt-1 fw-bold">
-						{{ number_format($productPercentageChange, 2) }}%
-					</span>
-					<span class="text-muted small pt-1 ps-1">
-						{{ $productPercentageChange >= 0 ? 'increase' : 'decrease' }}
-					</span>
-					@endrole
-				  </div>
-				</div>
-			  </div>
-
-			</div>
-		  </div><!-- End Product Card -->
-
 		  <!-- Supplier Card -->
 		  <div class="@role('admin') col-xxl-4 @endrole @role('staff') col-xxl-6 @endrole col-md-6">
 			<div class="card info-card supplier-card">
@@ -105,11 +63,9 @@
 			</div>
 		  </div><!-- End Supplier Card -->
 
-		  @role('admin')
-		  <!-- Request Card -->
-		  <div class="col-xxl-4 col-xl-12">
-
-			<div class="card info-card request-card">
+		   <!-- Product Card -->
+		  <div class="@role('admin') col-xxl-4 @endrole @role('staff') col-xxl-6 @endrole col-md-6">
+			<div class="card info-card product-card">
 
 			  <div class="filter">
 				<a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
@@ -118,26 +74,70 @@
 					<h6>Filter</h6>
 				  </li>
 
-				  <li><a class="dropdown-item" href="{{ route('dashboard', ['card' => 'request', 'period' => 'daily']) }}">Today</a></li>
-				  <li><a class="dropdown-item" href="{{ route('dashboard', ['card' => 'request', 'period' => 'monthly']) }}">This Month</a></li>
-				  <li><a class="dropdown-item" href="{{ route('dashboard', ['card' => 'request', 'period' => 'yearly']) }}">This Year</a></li>
+				  	<li><a class="dropdown-item" href="{{ route('dashboard', ['card' => 'product', 'period' => 'daily']) }}">Today</a></li>
+					<li><a class="dropdown-item" href="{{ route('dashboard', ['card' => 'product', 'period' => 'monthly']) }}">This Month</a></li>
+					<li><a class="dropdown-item" href="{{ route('dashboard', ['card' => 'product', 'period' => 'yearly']) }}">This Year</a></li>
+
 				</ul>
 			  </div>
 
 			  <div class="card-body">
-				<h5 class="card-title">Total Request <span>| {{ request()->get('period', 'daily') }}</span></h5>
+				<h5 class="card-title">Total Product <span>| {{ request()->get('period', 'daily') }}</span></h5>
+
+				<div class="d-flex align-items-center">
+				  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+					<i class="bi bi-box-fill"></i>
+				  </div>
+				  <div class="ps-3">
+					<h6>{{ $productCount }}</h6>
+					@role('admin')
+					<span class="{{ $productPercentageChange >= 0 ? 'text-success' : 'text-danger' }} small pt-1 fw-bold">
+						{{ number_format($productPercentageChange, 2) }}%
+					</span>
+					<span class="text-muted small pt-1 ps-1">
+						{{ $productPercentageChange >= 0 ? 'increase' : 'decrease' }}
+					</span>
+					@endrole
+				  </div>
+				</div>
+			  </div>
+
+			</div>
+		  </div><!-- End Product Card -->
+
+		  @role('admin')
+		  <!-- Order Card -->
+		  <div class="col-xxl-4 col-xl-12">
+
+			<div class="card info-card order-card">
+
+			  <div class="filter">
+				<a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+				<ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+				  <li class="dropdown-header text-start">
+					<h6>Filter</h6>
+				  </li>
+
+				  <li><a class="dropdown-item" href="{{ route('dashboard', ['card' => 'order', 'period' => 'daily']) }}">Today</a></li>
+				  <li><a class="dropdown-item" href="{{ route('dashboard', ['card' => 'order', 'period' => 'monthly']) }}">This Month</a></li>
+				  <li><a class="dropdown-item" href="{{ route('dashboard', ['card' => 'order', 'period' => 'yearly']) }}">This Year</a></li>
+				</ul>
+			  </div>
+
+			  <div class="card-body">
+				<h5 class="card-title">Total Order <span>| {{ request()->get('period', 'daily') }}</span></h5>
 
 				<div class="d-flex align-items-center">
 				  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
 					<i class="bi bi-arrow-left-right"></i>
 				  </div>
 				  <div class="ps-3">
-					<h6>{{ $requestCount }}</h6>
-					<span class="{{ $requestPercentageChange >= 0 ? 'text-success' : 'text-danger' }} small pt-1 fw-bold">
-						{{ number_format($requestPercentageChange, 2) }}%
+					<h6>{{ $orderCount }}</h6>
+					<span class="{{ $orderPercentageChange >= 0 ? 'text-success' : 'text-danger' }} small pt-1 fw-bold">
+						{{ number_format($orderPercentageChange, 2) }}%
 					</span>
 					<span class="text-muted small pt-1 ps-1">
-						{{ $requestPercentageChange >= 0 ? 'increase' : 'decrease' }}
+						{{ $orderPercentageChange >= 0 ? 'increase' : 'decrease' }}
 					</span>
 				  </div>
 				</div>
@@ -145,7 +145,7 @@
 			  </div>
 			</div>
 
-		  </div><!-- End Request Card -->
+		  </div><!-- End Order Card -->
 		  @endrole
 
 		  <!-- Reports -->
@@ -154,7 +154,7 @@
 			<div class="card">
 
 			  <div class="card-body">
-				<h5 class="card-title">Reports</h5>
+				<h5 class="card-title">Reports Daily</h5>
 
 				<!-- Line Chart -->
 				<div id="reportsChart"></div>
@@ -171,32 +171,32 @@
 			<!-- Recent Sales -->
 			<div class="card recent-sales overflow-auto">
 				<div class="card-body">
-				<h5 class="card-title">Recent Request</h5>
+				<h5 class="card-title">Recent Order</h5>
 
 				<table class="table table-borderless datatable">
 					<thead>
 					<tr>
 						<th scope="col">#</th>
-						<th scope="col">Request</th>
-						<th scope="col">Supplier</th>
+						<th scope="col">Product</th>
+						<th scope="col">Quantity</th>
 						<th scope="col">Deadline</th>
 						<th scope="col">Status</th>
 					</tr>
 					</thead>
 					<tbody>
-						@foreach($requests as $req)
+						@foreach($orders as $order)
 						<tr>
 						<td>{{ $loop->iteration }}</td>
-						<td>{{ $req->name }}</td>
-						<td>{{ $req->supplier->name }}</td>
-						<td>{{ $req->deadline }}</td>
+						<td>{{ $order->product->name }}</td>
+						<td>{{ $order->quantity }}</td>
+						<td>{{ $order->deadline }}</td>
 						<td>
-							@if ($req->status->value == 'pending')
-								<span class="badge bg-primary">{{ $req->status->name }}</span>
-							@elseif($req->status->value == 'accept')
-								<span class="badge bg-success">{{ $req->status->name }}</span>
-							@elseif($req->status->value == 'reject')
-								<span class="badge bg-danger">{{ $req->status->name }}</span>
+							@if ($order->status->value == 'pending')
+								<span class="badge bg-primary">{{ $order->status->name }}</span>
+							@elseif($order->status->value == 'accept')
+								<span class="badge bg-success">{{ $order->status->name }}</span>
+							@elseif($order->status->value == 'reject')
+								<span class="badge bg-danger">{{ $order->status->name }}</span>
 							@endif
 						</tr>
 						@endforeach
@@ -246,7 +246,7 @@
 								$color = match($activity->log_name) {
 									'product' => 'text-warning',
 									'supplier' => 'text-success',
-									'request' => 'text-primary',
+									'order' => 'text-primary',
 									default => 'text-secondary'
 								}
 							@endphp
@@ -286,13 +286,13 @@
 	document.addEventListener("DOMContentLoaded", () => {
 		const productData = @json($productData);
 		const supplierData = @json($supplierData);
-		const requestData = @json($requestData);
+		const orderData = @json($orderData);
 
 		const allDates = [
 			...new Set([
 				...productData.map(item => item.date),
 				...supplierData.map(item => item.date),
-				...requestData.map(item => item.date)
+				...orderData.map(item => item.date)
 			])
 		];
 
@@ -303,7 +303,7 @@
 
 		const productSeries = allDates.map(date => getDataForDate(productData, date));
 		const supplierSeries = allDates.map(date => getDataForDate(supplierData, date));
-		const requestSeries = allDates.map(date => getDataForDate(requestData, date));
+		const orderSeries = allDates.map(date => getDataForDate(orderData, date));
 
 		new ApexCharts(document.querySelector("#reportsChart"), {
 			series: [{
@@ -313,8 +313,8 @@
 				name: 'Supplier',
 				data: supplierSeries,
 			}, {
-				name: 'Request',
-				data: requestSeries,
+				name: 'Order',
+				data: orderSeries,
 			}],
 			chart: {
 				height: 350,
