dc731971482082428225a3f8bacda30d70ed6afb
jasen-devvv <janadevofficial@gmail.com>
2025-01-16 19:51:00 +0700

    fix: rename permission, add class, check null and integration sweetalert
    - Rename permission @can('view product') -> @can('create product').
    - Check null used @empty for data $product-description.
    - Rename Rp. -> IDR.
    - Integration sweetalert with add id="deleteForm" at tag form and class btn-delete at button submit.

--- src/resources/views/dashboard/product/index.blade.php
+++ src/resources/views/dashboard/product/index.blade.php
@@ -20,12 +20,12 @@
 
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">Products @can('view product') <a class="btn btn-success" href="{{ route('products.create') }}">Add <i class="bi bi-plus"></i></a>@endcan</h5>
+            <h5 class="card-title">Products @can('create product') <a class="btn btn-success" href="{{ route('products.create') }}">Add <i class="bi bi-plus"></i></a>@endcan</h5>
             <div class="alert alert-warning"><b>Note</b>: Please add a supplier first.</div>
 
             <!-- Table with stripped rows -->
             <div class="table-responsive">
-              <table class="table table-hover datatable">
+              <table class="table table-bordered table-striped table-hover datatable">
                 <thead>
                   <tr>
                     <th width="10%">No.</th>
@@ -46,8 +46,8 @@
                           <td>{{ $loop->iteration }}.</td>
                           <td>{{ $product->supplier->name }}</td>
                           <td>{{ $product->name }}</td>
-                          <td>{{ $product->description }}</td>
-                          <td>Rp. {{ $product->price }}</td>
+                          <td @empty($product->description) class="fst-italic" @endempty>{{ $product->description ?? "no description" }}</td>
+                          <td>IDR {{ $product->price }}</td>
                           @role('admin')
                           <td>
                               <div class="btn-group" role="group" aria-label="Action button">
@@ -56,10 +56,10 @@
                                   @endcan
 
                                   @can('delete product')
-                                  <form action="{{ route('products.destroy', $product->id) }}" method="POST">
+                                  <form id="deleteForm" action="{{ route('products.destroy', $product->id) }}" method="POST">
                                     @csrf
                                     @method("DELETE")
-                                    <button type="submit" class="btn btn-danger d-inline rounded-0 rounded-end">Delete</button>
+                                    <button type="submit" class="btn btn-danger btn-delete d-inline rounded-0 rounded-end">Delete</button>
                                   </form>
                                   @endcan
                               </div>
